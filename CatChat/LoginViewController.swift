64a1fe9413e00959817bc2ca6c6570fb38764166
Michael Chapman <submariner100@gmail.com>
2017-09-01 18:31:30 +0100

    first commit

---
+++ CatChat/LoginViewController.swift
@@ -0,0 +1,89 @@
+//
+//  LoginViewController.swift
+//  CatChat
+//
+//  Created by Macbook on 01/09/2017.
+//  Copyright © 2017 Chappy-App. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+
+class LoginViewController: UIViewController {
+	
+	@IBOutlet weak var emailTextField: UITextField!
+	@IBOutlet weak var passwordTextField: UITextField!
+	@IBOutlet weak var topButton: UIButton!
+	@IBOutlet weak var bottomButton: UIButton!
+	
+	var signupMode = false
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+	}
+	
+	@IBAction func topTapped(_ sender: Any) {
+		if let email = emailTextField.text {
+			if let password = passwordTextField.text {
+				if signupMode {
+					// Sign Up
+					FIRAuth.auth()?.createUser(withEmail: email, password: password, completion: { (user, error) in
+						if let error = error {
+							self.presentAlert(alert: error.localizedDescription)
+						} else {
+							print("Sign Up was successful :)")
+						}
+					})
+					
+		} else {
+			// Log In
+					FIRAuth.auth()?.signIn(withEmail: email, password: password, completion: { (user, error) in
+						if let error = error {
+							self.presentAlert(alert: error.localizedDescription)
+						} else {
+							print("Log In was successful :)")
+						}
+					})
+					
+				}
+				
+			}
+			
+		}
+	}
+	
+	func presentAlert(alert: String) {
+		let alertVC = UIAlertController(title: "Error", message: alert, preferredStyle: .alert)
+		let okAction = UIAlertAction(title: "Ok", style: .default) { (action) in
+			alertVC.dismiss(animated: true, completion: nil)
+		}
+		alertVC.addAction(okAction)
+		present(alertVC, animated: true, completion: nil)
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	@IBAction func bottomTapped(_ sender: Any) {
+		if signupMode {
+			// Switch to Log In
+			signupMode = false
+			topButton.setTitle("Log In", for: .normal)
+			bottomButton.setTitle("Switch to Sign Up", for: .normal)
+			
+		} else {
+			// Switch to Sign Up
+			signupMode = true
+			topButton.setTitle("Sign Up", for: .normal)
+			bottomButton.setTitle("Switch to Log In", for: .normal)
+			
+			
+		}
+	}
+}
+
